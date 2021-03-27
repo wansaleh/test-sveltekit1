6e4a06272548b67a442126900b09661ded88d13a
Wan Saleh <wansaleh@gmail.com>
2021-03-27 12:26:11 +0800

    Update.

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,30 +1,31 @@
-const sveltePreprocess = require("svelte-preprocess");
-const node = require('@sveltejs/adapter-node');
+const sveltePreprocess = require('svelte-preprocess');
+// const node = require('@sveltejs/adapter-node');
+const vercel = require('@sveltejs/adapter-vercel');
 const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
-	preprocess: [
-		sveltePreprocess({
-			defaults: {
-				style: "postcss",
-			},
-			postcss: true
-		}),
-	],
-	kit: {
-		// By default, `npm run build` will create a standard Node app.
-		// You can create optimized builds for different platforms by
-		// specifying a different adapter
-		adapter: node(),
+  preprocess: [
+    sveltePreprocess({
+      defaults: {
+        style: 'postcss',
+      },
+      postcss: true,
+    }),
+  ],
+  kit: {
+    // By default, `npm run build` will create a standard Node app.
+    // You can create optimized builds for different platforms by
+    // specifying a different adapter
+    adapter: vercel(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
 
-		vite: {
-			ssr: {
-				noExternal: Object.keys(pkg.dependencies || {})
-			}
-		}
-	}
+    vite: {
+      ssr: {
+        noExternal: Object.keys(pkg.dependencies || {}),
+      },
+    },
+  },
 };
